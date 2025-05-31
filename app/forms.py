9af14c359968a304e9413a38d84fff45efae8b4e
Leo <leo.obkirchner@tum.de>
2025-05-31 13:29:33 +0200

    fix some minor errors

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
